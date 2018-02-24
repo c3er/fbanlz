7c05b8a15e590d95aca799986a84d128c054c3a6
Christian Dreier <flipper.x3r@googlemail.com>
2018-02-24 20:57:05 +0100

    Get date strings of logons

--- fbanlz.py
+++ fbanlz.py
@@ -2,8 +2,29 @@
 # -*- coding: utf-8 -*-
 
 
+import sys
+import os
+import xml.etree.ElementTree as et
+
+
+def getstarterdir():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+
+def extract_datestrings(file):
+    root = et.parse(file).getroot()
+
+    # "element.text" returns only the first text node.
+    # Because the text nodes are splitted by "<br />" tags, they would be retrieved by calling
+    # "element.tail" of these "<br />" tags.
+    return [element.text for element in root.findall("body/div[@class='contents']/ul[2]/li/p")]
+
+
 def main():
-    pass
+    session_file = os.path.join(getstarterdir(), "fbdata", "html", "security.htm")
+    datestrings = extract_datestrings(session_file)
+    for date in datestrings:
+        print(date)
 
 
 if __name__ == "__main__":
